2b9a61e07d956c3eee0951a28905ff7454563935
Gerardofl <gerardo.rfll@gmail.com>
2020-08-13 22:36:52 -0500

    Add cahe

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+f <- NULL
+  set <- function(y) {
+    x <<- y
+    f <<- NULL
+  }
+  get <- function() x
+  set_inver <- function(inver) f <<- inver
+  get_inver <- function() f
+  list(set = set,
+       get = get,
+       set_inver = set_inver,
+       get_inver = get_inver)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+f <- x$get_inver()
+  if (!is.null(f)) {
+    mensaje("data del caché")
+    return(f)
+  }
+  dat <- x$get()
+  f <- solve(dat, ...)
+  x$set_inver(f)
+  f
 }
